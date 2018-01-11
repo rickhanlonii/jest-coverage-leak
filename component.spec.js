84c4bb65c8dcd1142bc6777b4253cdc24c834e25
rickhanlonii <rickhanlonii@gmail.com>
2018-01-11 11:13:24 -0500

    Update script to it's clear that only coverage changes

--- component.spec.js
+++ component.spec.js
@@ -1,8 +1,8 @@
-import React from 'react';
 import Component from './component.js';
 
 describe('Component', () => {
   it('should render with no props', () => {
+    // Notice that the component isn't even rendered
     expect(true).toBeTruthy();
   });
 });
