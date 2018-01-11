1ddd09acde2621247ecd9d31ade67af0dc9ca753
rickhanlonii <rickhanlonii@gmail.com>
2018-01-11 11:08:07 -0500

    Init

---
+++ component.spec.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Component from './component.js';
+
+describe('Component', () => {
+  it('should render with no props', () => {
+    expect(true).toBeTruthy();
+  });
+});
